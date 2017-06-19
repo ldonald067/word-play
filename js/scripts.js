06253cbd4c81771f9e33ebdf65efae6937652b15
LaTanya Donaldson <ldonald234@gmail.com>
2017-06-18 17:20:55 -0700

    polish code

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,23 @@
-$(document).ready(function() {
-$("#formOne").submit(function(event) {
-  event.preventDefault();
-  var inputWords = $("input#item1").val();
-  var splitWords= inputWords.split(" ");
-  //if ()branch for eliminating small words?
-  alert(splitWords);
+$ (function() {
+  $("#form").submit(function(event){
+    var userInput = $("#item1").val();
 
-});
+    var userInput = userInput.split(" ");
+
+    var sentence = userInput.map(function(input) {
+      if (input.length >= 3) {
+        return input;
+      }
+      else {
+        ;
+      }
+    });
+    sentence.reverse();
+    sentence = sentence.join(" ");
+
+    $("ul").text(sentence);
+
+    event.preventDefault();
+
+  });
 });
